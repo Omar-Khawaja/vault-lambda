cc6670d85daba05c6a9314fb3a758f621cadb5a8
Omar Khawaja <khawaja910@gmail.com>
2020-03-18 13:05:54 -0400

    replace vault token environment variable with code that fetches this info from AWS secrets manager

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	"github.com/hashicorp/vault/api"
 )
 
@@ -24,8 +28,38 @@ type EC2LifecycleHookEvent struct {
 	Detail EC2LifecycleHookEventDetail `json:"detail"`
 }
 
+// SecretInfo holds the root token and recovery keys
+type secretInfo struct {
+	RootToken    string
+	RecoveryKeys []string
+}
+
 func removePeerHandler(event EC2LifecycleHookEvent) error {
 	log.Println("removePeerHandler has been initiated...")
+
+	region := os.Getenv("awsRegion")
+	secretID := os.Getenv("secretID")
+
+	secretinfo := &secretInfo{}
+	svc := secretsmanager.New(session.New(), aws.NewConfig().WithRegion(region))
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretID),
+	}
+
+	output, err := svc.GetSecretValue(input)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	secretString := *output.SecretString
+	err = json.Unmarshal([]byte(secretString), secretinfo)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	vaultToken := secretinfo.RootToken
+
 	config := &api.Config{
 		Address: os.Getenv("VAULT_ADDR"),
 	}
@@ -33,7 +67,8 @@ func removePeerHandler(event EC2LifecycleHookEvent) error {
 	if err != nil {
 		log.Fatal(err)
 	}
-	client.SetToken(os.Getenv("VAULT_TOKEN"))
+
+	client.SetToken(vaultToken)
 	logical := client.Logical()
 
 	// The Terraform that spins up the Vault cluster configures the Vault
